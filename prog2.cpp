adbe2abe87bddbd11820423d215d065c72dd2b37
Christina L <christina511ny@gmail.com>
2018-03-23 17:36:06 -0400

    Create prog2.cpp

---
+++ prog2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// prog2 to print name and address
+// Feb 10, 2018
+int main(){
+			cout<<"\t\t\tChristina Low\n";
+			cout<<"\t\t\t12-34 124th st\n";
+			cout<<("\t\t\tFake Place NY\n");
+			cout<<("\t\t\t12345\n");
+			//system("pause");
+	return 0;
+}
