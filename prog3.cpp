7acecf22bb868ad37635b9d0363ae6abd80e07c9
Christina L <christina511ny@gmail.com>
2018-03-23 17:37:12 -0400

    Create prog3.cpp

---
+++ prog3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+// prog3 to print centered paragraph
+// Feb 10, 2018
+int main(){
+			cout<<"\t*****************************************************************\n";
+			cout<<"\t*\t\t\t     Summary:\t\t\t\t*\n";
+			cout<<"\t*\t\t\t\t\t\t\t\t*\n";
+			cout<<"\t*     There are three parts to a program in the C++ language.\t*\n";
+			cout<<"\t*    The first is that there are user words, or identifiers,\t*\n";
+			cout<<"\t*\twhich are represented by the colors black and blue.\t*\n";
+			cout<<"\t*\t\t\t\t\t\t\t\t*\n";
+			cout<<"\t*\t\tSecond is the comments, or remarks.\t\t*\n";
+			cout<<"\t*\tIt only helps for the programmer or the viewer.\t\t*\n";
+			cout<<"\t*\t\tIt is represented by the color green.\t\t*\n";
+			cout<<"\t*\t\t\t\t\t\t\t\t*\n";
+			cout<<"\t*\tThird ia the keywords/reserved words.\t*\n";
+			cout<<"\t*****************************************************************\n";
+			//system("pause");
+	return 0;
+}
+
+/*Summary:*/
