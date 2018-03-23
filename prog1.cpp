02e745a6414dacf8e918a0128700c8536fdd431b
Christina L <christina511ny@gmail.com>
2018-03-23 17:34:24 -0400

    Create prog1.cpp

---
+++ prog1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+// prog1 to print " Welcome to C++ World "
+// Feb 10, 2018
+int main(){
+			cout<<"Welcome to C++ World\n";
+			system("pause");
+	return 0;
+}
