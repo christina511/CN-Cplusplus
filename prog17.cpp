6ac6dd3f40776f4fc16e6b1bd035de2a3289ee6c
Christina L <christina511ny@gmail.com>
2018-03-23 17:43:49 -0400

    Create prog17.cpp

---
+++ prog17.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//p17 March 17, 2018
+int main(){
+	int a;
+	cout<<"Enter a number: ";
+	cin>>a;
+	if(a==3)
+		cout<<"three"<<endl;
+	else if(a==5)
+		cout<<"five"<<endl;
+	else if(a==7)
+		cout<<"seven"<<endl;
+	else
+		cout<<"Others\n";
+	return 0;
+}
