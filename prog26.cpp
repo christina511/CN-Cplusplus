a09e2669cd68660e8f36b59ab049221e0ecf46af
Christina L <christina511ny@gmail.com>
2018-03-24 11:21:31 -0400

    Create prog26.cpp

---
+++ prog26.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p26 is p22 using do-while loop
+//March 24, 2018
+int main(){
+	int w;
+	w=1;
+	do{
+		cout<<"Christina\n";
+		w++;
+		}while(w<=5);
+	return 0;
+}
