df284c328ba76b611b048bff4270ae6a067e5da4
Christina L <christina511ny@gmail.com>
2018-03-24 11:14:03 -0400

    Create prog21.cpp

---
+++ prog21.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p21 to print your name 5 times
+//March 24,2018
+int main(){
+	cout<<"Christina\n";
+	cout<<"Christina\n";
+	cout<<"Christina\n";
+	cout<<"Christina\n";
+	cout<<"Christina\n";
+	return 0;
+	//note- not convenient since you have to output each one manually.
+}
