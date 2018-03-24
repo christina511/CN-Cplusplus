69471085f7fcaa5fc5f67da7a2af51bb8f22bf50
Christina L <christina511ny@gmail.com>
2018-03-24 16:01:39 -0400

    Update prog20.cpp

--- prog20.cpp
+++ prog20.cpp
@@ -22,3 +22,24 @@ int main(){
     }
     return 0;
 }
+
+//this was Mr. Elhari's way to do it! Since it's already defined as an integer, when we divide by 10, it works out!
+//int main(){
+//     int G;
+//     cout<<"Enter a score: ";
+//     cin>>G;
+//     switch(G/10){
+//         case 10: cout<<"Your grade is an A\n";
+//             break;
+//         case 9: cout<<"Your grade is an A\n";
+//             break;
+//         case 8: cout<<"Your grade is a B\n";
+//             break;
+//         case 7: cout<<"Your grade is a C\n";
+//             break;
+//         case 6: cout<<"Your grade is a D\n";
+//             break;
+//         default: cout<<"Your grade is a F, sorry :(\n";
+//     }
+//     return 0;
+// }
