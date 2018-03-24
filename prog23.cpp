860cf603eebb3c5498971fa1dd74184a266112ca
Christina L <christina511ny@gmail.com>
2018-03-24 11:14:45 -0400

    Create prog23.cpp

---
+++ prog23.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p23 to print #'s from 1 to 19
+//March 24,2018
+int main(){
+	int w;
+	w=1;
+	while(w<=19){
+		cout<<w<<endl;
+		w+=1;
+	}
+	return 0;
+}
