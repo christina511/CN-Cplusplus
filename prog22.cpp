d54f06e6abfff54c0ff1fd0b37ec0e7f0363963a
Christina L <christina511ny@gmail.com>
2018-03-24 11:14:24 -0400

    Create prog22.cpp

---
+++ prog22.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p22 is p21 using while loop
+//March 24,2018
+int main(){
+	int w;
+	w=1; //initial value
+	while(w<=5){
+		cout<<"Christina\n";
+		w=w+1; //terminate
+	}
+	return 0;
+}
