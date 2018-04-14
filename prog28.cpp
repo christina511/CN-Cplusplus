5cbf45bf7658535717375d2a662f503cd7b3c987
Christina L <christina511ny@gmail.com>
2018-04-13 20:54:28 -0400

    Create prog28.cpp

---
+++ prog28.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//p28 is to print odd numbers from 19 to -5
+//March 24,2018
+int main(){
+	int w;
+	w=19;
+	while(w>=-5){
+		if(w%2!=0)
+			cout<<w<<endl;
+	w--;
+	}
+	return 0;
+}
