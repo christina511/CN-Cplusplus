6c7ce11b49d1aab700a4dfb545dd4d485a8d822d
Christina L <christina511ny@gmail.com>
2018-04-14 10:07:25 -0400

    Create prog44.cpp

---
+++ prog44.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//p44 is to count how many -7 exists in the list
+//spring break hw due April 14, 2018
+int main(){
+    int w,num,l,count;
+    w=1;
+    count=0;
+    cout<<"List length: ";
+    cin>>l;
+    cout<<"Enter the #'s from the list:";
+    while(w<=l){
+        cin>>num;
+        if (num==-7)
+            count++
+        w++;
+    }
+      cout<<"There is/are <<count<<" -7's in the list\n";
+    return 0;
+}
