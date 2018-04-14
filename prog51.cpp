81ed7ce5ebe6cb808298a29127742be137f461fb
Christina L <christina511ny@gmail.com>
2018-04-14 10:35:24 -0400

    Create prog51.cpp

---
+++ prog51.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//p51 is to print every other number between 1 and 10 starting from 2 (p34) using FOR loop
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    for(w=1;w<=10;w++)
+      if(w%2==0)
+        cout<<w<<endl;
+    return 0;
+}
