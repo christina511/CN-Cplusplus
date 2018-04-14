2f875b123f526d084aa6c6eeeff70c3cbfa112e1
Christina L <christina511ny@gmail.com>
2018-04-14 00:01:33 -0400

    Create prog35.cpp

---
+++ prog35.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p35 is to print the first and last numbers from 13 to 17
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    w=13;
+    cout<<w<<endl;
+    while(w<17)
+        w++;
+    cout<<w<<endl;
+    return 0;
+}
