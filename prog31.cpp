811d152aa6fe45291b970c8410266fa17aafcb14
Christina L <christina511ny@gmail.com>
2018-04-13 20:58:14 -0400

    Create prog31.cpp

---
+++ prog31.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+//p31 to print the sum of even #'s between 19 and 21
+//March 24,2018
+int main(){
+	int w, sum;
+	w=19;
+	sum=0;
+	while (w<=21){
+			
+	}
+}
