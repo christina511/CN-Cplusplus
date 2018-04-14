6c1de77b05371535086f31380950da3d1800e65e
Christina L <christina511ny@gmail.com>
2018-04-14 09:34:22 -0400

    Update prog35.cpp

--- prog35.cpp
+++ prog35.cpp
@@ -11,3 +11,21 @@ int main(){
     cout<<w<<endl;
     return 0;
 }
+
+/* alternate method
+#include<iostream>
+using namespace std;
+//p35 is to print the first and last numbers from 13 to 17
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    w=13;
+    while(w<=17)
+        if (w==13)
+            cout<<"The first is "<<w<<endl;
+        if (w==17)
+            cout<<"The last is "<<w<<endl;
+        w++;
+    return 0;
+}
+*/
