d05e636f24a0df0efe3193a706a46830f80adb0a
Christina L <christina511ny@gmail.com>
2018-04-14 09:24:27 -0400

    Create prog33withdowhile.cpp

---
+++ prog33withdowhile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//p33 is to print every other number between 1 and 10 starting from 1
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    w=1;
+    do{
+        cout<<w<<endl;
+        w=w+2;
+    }while(w<=10);
+    return 0;
+}
