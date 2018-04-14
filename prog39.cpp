0fc6a00279c7fc14232723adef1560b19724bcf7
Christina L <christina511ny@gmail.com>
2018-04-14 00:47:22 -0400

    Create prog39.cpp

---
+++ prog39.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//p39 is to print the negative numbers of a list of 5 numbers: 3, 2, -7, 19, -18 (-19 to -2)
+//spring break hw due April 14, 2018
+int main(){
+    int w,num;
+    w=1;
+    cout<<"Enter the #'s from the list:";
+    while(w<=5){
+        cin>>num;
+        if (num<=-2 and num>= -19)
+            cout<<num<<endl;
+        w++;
+    }
+    return 0;
+}
