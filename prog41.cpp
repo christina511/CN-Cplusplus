4ee4f8455417ef7ba14176f2309dc380da031ca1
Christina L <christina511ny@gmail.com>
2018-04-14 00:56:08 -0400

    Update prog41.cpp

--- prog41.cpp
+++ prog41.cpp
@@ -3,14 +3,16 @@ using namespace std;
 //p41 is to print the first and last numbers of the list
 //spring break hw due April 14, 2018
 int main(){
-    int w,num;
+    int w,num,l;
     w=1;
+    cout<<"List length: ";
+    cin>>l;
     cout<<"Enter the #'s from the list:";
-    while(w<=5){
+    while(w<=l){
         cin>>num;
         if (w==1)
             cout<<num<<endl;
-        else if (w==5)
+        else if (w==l)
             cout<<num<<endl;
         w++;
     }
