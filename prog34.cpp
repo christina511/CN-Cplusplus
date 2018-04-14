2fe53bcf0db40ac31c7a9c3ae6da9fed9faecd2a
Christina L <christina511ny@gmail.com>
2018-04-14 10:32:18 -0400

    Create prog34.cpp

---
+++ prog34.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//p34 is to print every other number between 1 and 10 starting from 2
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    w=1;
+    while(w<=10){
+        if(w%2==0)
+          cout<<w<<endl;
+        w++;
+    }
+    return 0;
+}
