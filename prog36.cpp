cc5277a53498161163c909f4490da8c6df39154d
Christina L <christina511ny@gmail.com>
2018-04-14 00:06:05 -0400

    Create prog36.cpp

---
+++ prog36.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//p36 is to print a list of 5 numbers: 3, 2, -7, 19, -18
+//spring break hw due April 14, 2018
+int main(){
+    int w,num;
+    w=1;
+    cout<<"Enter the #'s from the list:";
+    while(w<=5){
+        cin>>num;
+        cout<<num<<endl;
+        w++;
+    }
+    return 0;
+}
