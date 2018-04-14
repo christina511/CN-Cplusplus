20364f74380fb6ccb022de0a6f7894ba5b7aae42
Christina L <christina511ny@gmail.com>
2018-04-13 20:53:27 -0400

    Create prog27.cpp
    3/24/18

---
+++ prog27.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//p27 to print even # from -5 to 19
+//March 24,2018
+int main(){
+	int w;
+	w=-5;
+	while(w<=19){
+		if(w%2==0)
+			cout<<w<<endl;
+	w+=1;
+	}
+return 0;
+}
