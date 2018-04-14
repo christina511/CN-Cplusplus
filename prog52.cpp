93ae53aa67b6bbc9b6a91373170d21de69e5ab8b
Christina L <christina511ny@gmail.com>
2018-04-14 10:47:38 -0400

    Create prog52.cpp

---
+++ prog52.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//p52 is to print the first and last numbers from 13 to 17 (p35) using FOR loop
+//spring break hw due April 14, 2018
+int main(){
+    int w;
+    for(w=13;w<=17;w++){
+        if (w==13)
+            cout<<"The first is "<<w<<endl;
+        if (w==17)
+            cout<<"The last is "<<w<<endl;
+      }
+    return 0;
+}
